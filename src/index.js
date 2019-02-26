8e111730c2411ed109a9e0d137bd6cc04c508c34
Andre Braga <andrebragabb@gmail.com>
2019-02-26 20:40:46 +0000

    seasons

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
+
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderComponent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Loader message="please accept to share your location" />;
+  }
+
+  render() {
+    return <div>{this.renderComponent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
